2a2bd695cfb74fe13c47da6f25c5dc7ef8e010d7
Oktay Ibis <oktayibis@gmail.com>
2020-07-12 21:41:44 +0300

    pikey changed

--- src/context/total.provider.js
+++ src/context/total.provider.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import TotalContext from "./total.context";
 
 const API_URL = "https://api.collectapi.com/economy/goldPrice";
-const API_KEY = "apikey 5Rhb0GyJKN52GGRC8vgxoi:5laVnsyYDGBN6pvQX9Eh8C";
+const API_KEY = "apikey 6ir98Q5slIwaWaRHHoqNFr:4rUydREoQA4XeR0cEw3Wsh";
 
 const TotalProvider = ({ children }) => {
 	const [totalPrice, setTotalPrice] = useState(0);
